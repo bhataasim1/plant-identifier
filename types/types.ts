faf821bde1d6042393ffeba961476b41b09982c9
bhataasim1 <bhataasim700@gmail.com>
2024-08-09 12:04:50 +0530

    feat: Add types for PlantInfo and ImageUpload components

---
+++ types/types.ts
@@ -0,0 +1,18 @@
+export interface PlantInfoType {
+  name: string;
+  scientificName: string;
+  family: string;
+  nativeRegion: string;
+  description: string;
+  careInstructions: string;
+}
+
+export interface PlantInfoProps {
+  info: PlantInfoType;
+  imageUrl: string | null;
+}
+
+export interface ImageUploadProps {
+  setPlantInfo: (info: PlantInfoType | null) => void;
+  setImageUrl: (url: string | null) => void;
+}
