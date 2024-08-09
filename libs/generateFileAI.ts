92812a67d688d31fe7130c5c30d642dfb1044584
bhataasim1 <bhataasim700@gmail.com>
2024-08-09 12:04:40 +0530

    feat: Add fileToGenerativePart function for generating generative part from file

---
+++ libs/generateFileAI.ts
@@ -0,0 +1,11 @@
+export async function fileToGenerativePart(file: File) {
+  const base64EncodedDataPromise = new Promise<string>((resolve) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve((reader.result as string).split(",")[1]);
+    reader.readAsDataURL(file);
+  });
+
+  return {
+    inlineData: { data: await base64EncodedDataPromise, mimeType: file.type },
+  };
+}
