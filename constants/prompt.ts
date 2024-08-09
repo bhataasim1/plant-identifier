7653bc9837cb906eb5eb47a8c3f9d72b49228cf1
bhataasim1 <bhataasim700@gmail.com>
2024-08-09 12:04:36 +0530

    feat: Add PROMPT constant for plant identification form

---
+++ constants/prompt.ts
@@ -0,0 +1,2 @@
+export const PROMPT =
+  "Identify this plant and provide the following information in JSON format: Plant name, Scientific name, Plant family, Native region, Brief description, Care instructions.";
