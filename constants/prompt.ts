d45ff827490d2ebf22286a6a0d12865f256af6bd
bhataasim1 <bhataasim700@gmail.com>
2024-09-02 11:56:12 +0530

    feat: Add medicinal value to plant identification prompt

--- constants/prompt.ts
+++ constants/prompt.ts
@@ -1,2 +1,2 @@
 export const PROMPT =
-  "Identify this plant and provide the following information in JSON format: Plant name, Scientific name, Plant family, Native region, Brief description, Care instructions.";
+  "Identify this plant and provide the following information in JSON format: Plant name, Scientific name, Plant family, Native region, Brief description, Care instructions, medicinal value.";
